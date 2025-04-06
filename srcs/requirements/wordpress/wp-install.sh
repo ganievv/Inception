54cdcf5dd8dc9d824ce48fdcdd88ceae7d4c5509
Semen Ganiev <sganiev@student.42heilbronn.de>
2025-04-06 17:24:02 +0200

    added script for wordpress initialization

---
+++ srcs/requirements/wordpress/wp-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+WP_PASSWORD=$(cat "${WP_PASSWORD_FILE}")
+WP_ROOT_PASSWORD=$(cat "${WP_ROOT_PASSWORD_FILE}")
+
+if ! wp core is-installed --path=/wordpress --allow-root ; then
+	wp core install \
+	--url="https://${DOMAIN_NAME}" \
+	--title="${TITLE}" \
+	--admin_user="${WP_ROOT_USER}" \
+	--admin_password="${WP_ROOT_PASSWORD}" \
+	--admin_email="${WP_ROOT_USER_EMAIL}" \
+	--path=/wordpress \
+	--allow-root
+
+	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
+	--user_pass="${WP_PASSWORD}" \
+	--role=editor \
+	--path=/wordpress \
+	--allow-root
+fi
+
+exec php-fpm7.4 -F
