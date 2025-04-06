693d19d1ef12b49e04d39be629cf7576de2799fa
Semen Ganiev <sganiev@student.42heilbronn.de>
2025-04-06 18:35:58 +0200

    allow direct theme/plugin installation in WordPress

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -40,6 +40,8 @@ $table_prefix = 'wp_';
 
 define( 'WP_DEBUG', false );
 
+define( 'FS_METHOD', 'direct' );
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
